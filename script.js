06d2254b00184a9d40a54e5a498b9fbf31d8047a
flenoops <flenchikv@gmail.com>
2025-01-24 22:07:52 +0200

    z3

--- script.js
+++ script.js
@@ -24,3 +24,17 @@
       document.body.classList.toggle("no-scroll");
     }
   })();
+
+
+  const btnMenu = document.querySelector(".header_right--mobile--menu")
+  const mobileMenu = document.querySelector(".header_right--mobile--menu--list--wrapper")
+
+  btnMenu.addEventListener("click", () => {
+    mobileMenu.style.transform ="translateX(0)"
+  }) 
+  
+  document.addEventListener("click", (event) => {
+  if(!mobileMenu.contains(event.target) && !btnMenu.contains(event.target)) {
+    mobileMenu.style.transform ="translateX(100%)"
+  }
+  }) 
